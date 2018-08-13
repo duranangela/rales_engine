1377386c49218fc004919f8397e2e36a095d72df
ClaireBvs <clairembeauvais@gmail.com>
2018-08-13 17:49:13 -0600

    add gem shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,13 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
+
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
