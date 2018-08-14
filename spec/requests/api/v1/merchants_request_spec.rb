8e94e349947c0fbff0d45e1c2bbcd148ab520019
Angela Duran <duranangela19@gmail.com>
2018-08-13 20:49:10 -0600

    Add index route and action for merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,24 @@ describe 'Merchants API' do
       get '/api/v1/merchants'
 
       expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      merchant = merchants.first
+
+      expect(merchants.count).to eq(3)
+      expect(merchant).to have_key(:name)
+    end
+  end
+  context 'GET /api/v1/merchants/:id' do
+    it 'can get one merchant by its id' do
+      id = create(:merchant).id
+
+      get "/api/v1/merchants/#{id}"
+
+      merchant = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(merchant["id"]).to eq(id)
     end
   end
 end
