790a56d49a791cac627d8c309e1ba6a48f269c04
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:24:25 -0600

    add validation test for Item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:price)}
+  end
 end
