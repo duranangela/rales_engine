781f2eb2c08914757de7357729675c081eeb52eb
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:58:30 -0600

    refactor request test for invoice

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Invoices API" do
   it "sends a list of invoices" do
-    customer = Customer.create()
-    create(:merchant)
-    create_list(:invoice, 3)
+    customer1 = create(:customer)
+    merchant1 = create(:merchant)
+    create_list(:invoice, 3, customer_id: customer1.id, merchant_id: merchant1.id)
 
     get '/api/v1/invoices'
 
