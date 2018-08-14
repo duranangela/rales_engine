318b444e63c249fbd15df9f47c5f92faab896dc6
ClaireBvs <clairembeauvais@gmail.com>
2018-08-13 21:34:22 -0600

    add data for merchant

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-describe 'Merchant', type: :model do
-  describe 'validations' do
-    it {should validate_presence_of(:name)}
-  end
+RSpec.describe Merchant, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of(:name)}
 end
