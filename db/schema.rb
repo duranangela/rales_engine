92dbf214dbbb517435b1a33ef73d86968fa260b2
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:06:59 -0600

    add model and table for transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814143944) do
-
+ActiveRecord::Schema.define(version: 20180814190402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +38,17 @@ ActiveRecord::Schema.define(version: 20180814143944) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
   add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
-
+  add_foreign_key "transactions", "invoices"
 end
