0d98754f0853d4e58223359aa4ed50d1f1c39078
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:29:38 -0600

    add relationships test for invoice

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Invoice, type: :model do
   describe 'validations' do
     it {should validate_presence_of(:status)}
   end
+
+  describe 'relationships' do
+    it {should have_many(:invoice_items)}
+    it {should have_many(:items).through(:invoice_items)}
+  end
 end
