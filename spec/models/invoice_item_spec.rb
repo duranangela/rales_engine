627dc073118f38bf72b27ff8f1941ff4e9401660
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:27:25 -0600

    add validations test for InvoiceItem

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of(:quantity)}
+    it {should validate_presence_of(:unit_price)}
+  end
 end
