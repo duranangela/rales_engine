b2c627bfdf649d52cbdf7cc2efd42bab7c455805
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 13:31:56 -0600

    add relationship test for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of(:description)}
     it {should validate_presence_of(:price)}
   end
+
+  describe 'relationships' do
+    it {should have_many(:invoice_items)}
+    it {should have_many(:invoices).through(:invoice_items)}
+  end
 end
