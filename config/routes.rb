bf51232a25c7bee3d07997aed54145654d540fb0
ClaireBvs <clairembeauvais@gmail.com>
2018-08-14 18:33:31 -0600

    add record endpoint index for invoices

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
       get 'merchants/:id/favorite_customer', to: 'merchants/favorite_customer#show'
+
       resources :merchants, only: [:index, :show]
-      # resources :merchants, path: "/api/v1/merchants/#{id}/favorite_customer"
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index]
     end
