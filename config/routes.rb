e7d6373e3c6b0414f20d399985647d19885667e6
Angela Duran <duranangela19@gmail.com>
2018-08-15 12:02:17 -0600

    Add route and action for merchant find all

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+      end
 
       get 'merchants/:id/favorite_customer', to: 'merchants/favorite_customer#show'
 
