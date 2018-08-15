74e93eb7f2bb0b82b66dad4608840d76a03b7cf1
ClaireBvs <clairembeauvais@gmail.com>
2018-08-15 16:33:42 -0600

    add method to find favorite customer for merchant

--- app/controllers/api/v1/merchants/favorite_customer_controller.rb
+++ app/controllers/api/v1/merchants/favorite_customer_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::Merchants::FavoriteCustomerController < ApplicationController
 
   def show
-    render json: Merchant.find(params[:id]).favorite_customer_for_merchant
+    render json: Customer.favorite_customer_for_merchant(params[:id])
   end
+
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,12 +6,4 @@ class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
 
-  def favorite_customer_for_merchant
-    # require "pry"; binding.pry
-    Customer.select('customers.*, count(invoices.id) AS invoice_customer').
-            joins(:invoices, :merchants, :transactions).
-            where(transactions: {result: 'success' })
-            group("customers.id").
-            order('invoice_customer DESC').limit(1).first
-  end
 end
