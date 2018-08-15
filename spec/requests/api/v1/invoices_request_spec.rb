eda59819f420659c0ca8b40b8fc3b1fb59833c44
ClaireBvs <clairembeauvais@gmail.com>
2018-08-15 17:14:08 -0600

    add test invoice request find by id

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -28,5 +28,19 @@ describe "Invoices API" do
     end
   end
 
-  
+  context 'GET/api/v1/invoices/find?id=2' do
+    it 'can find invoice by parameter id' do
+      customer1 = create(:customer)
+      merchant1 = create(:merchant)
+      invoice_1 = create(:invoice, id: 1, merchant_id: merchant1.id, customer_id: customer1.id)
+      invoice_2 = create(:invoice, id: 2, merchant_id: merchant1.id, customer_id: customer1.id)
+
+      get "/api/v1/invoices/find?id=2"
+
+      invoice = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(invoice["id"]).to eq(invoice_2.id)
+    end
+  end
 end
