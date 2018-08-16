ed888a78e8d266e08df29d0ac47c8451eff59be7
ClaireBvs <clairembeauvais@gmail.com>
2018-08-15 23:27:27 -0600

    add test items all

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Items API" do
+  context 'GET /api/v1/items' do
+    it "sends a list of items" do
+      merchant = create(:merchant)
+      create_list(:item, 5, merchant_id: merchant.id)
+
+      get '/api/v1/items'
+
+      expect(response.to be_successful)
+    end
+  end
+end
