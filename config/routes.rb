9f8aaf4c11bcf66a71aefb5ca7defad99e26d465
ClaireBvs <clairembeauvais@gmail.com>
2018-08-16 00:03:56 -0600

    add method show for invoiceitem

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
       resources :invoices, only: [:index, :show]
       resources :transactions, only: [:index, :show]
       resources :items, only: [:index, :show]
-      resources :invoice_items, only: [:index]
+      resources :invoice_items, only: [:index, :show]
 
     end
   end
