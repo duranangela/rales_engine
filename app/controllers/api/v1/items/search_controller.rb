8764c9062691eeb9f7ae42249f4cb7bd3afbcc22
ClaireBvs <clairembeauvais@gmail.com>
2018-08-15 23:47:05 -0600

    add items find all

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::Items::SearchController < ApplicationController
 
+  def index
+    render json: Item.where(search_params)
+  end
+
   def show
     render json: Item.find_by(search_params)
   end
