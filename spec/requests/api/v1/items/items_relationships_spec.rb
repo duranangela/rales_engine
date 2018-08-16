c77cbb455f3bdbc241e5873aadfbb6ab7f93120b
Angela Duran <duranangela19@gmail.com>
2018-08-15 19:25:52 -0600

    Add item relationship test for invoice_items

--- spec/requests/api/v1/items/items_relationships_spec.rb
+++ spec/requests/api/v1/items/items_relationships_spec.rb
@@ -15,4 +15,21 @@ describe 'Items Relationships API' do
       expect(merchant["id"]).to eq(id)
     end
   end
+  context 'GET /api/v1/items/:id/invoice_items' do
+    it 'gets the associated invoice_items for an item' do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      item = create(:item, merchant_id: merchant.id)
+      invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      create_list(:invoice_item, 4, item_id: item.id, invoice_id: invoice.id)
+
+      get "/api/v1/items/#{item.id}/invoice_items"
+
+      expect(response).to be_successful
+
+      invoice_items = JSON.parse(response.body)
+
+      expect(invoice_items.count).to eq(4)
+    end
+  end
 end
