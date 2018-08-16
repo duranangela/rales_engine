a75468a6d4d0a528d6167e4e31dca830bf19fce3
Angela Duran <duranangela19@gmail.com>
2018-08-16 07:22:56 -0600

    Add customer record endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,12 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
       end
 
+      namespace :customers do
+        get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
+        get '/random', to: 'random#show'
+      end
+
       resources :merchants, only: [:index, :show]
       resources :customers, only: [:index, :show]
       resources :invoices, only: [:index, :show]
