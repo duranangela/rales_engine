eb2e3ec1813012afa2519313b842695e9a802c0e
ClaireBvs <clairembeauvais@gmail.com>
2018-08-16 17:42:55 -0600

    refactor endpoint invoice item for unit price

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -2,4 +2,8 @@ class InvoiceItemSerializer < ActiveModel::Serializer
 
   attributes :id, :invoice_id, :item_id, :quantity, :unit_price
 
+  def unit_price
+    (object.unit_price * 0.01.round(2)).to_s
+  end
+
 end
