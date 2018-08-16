8d2f463ddaeea2a5012a7ef54a766c91f28e7874
Angela Duran <duranangela19@gmail.com>
2018-08-16 12:42:00 -0600

    Add database cleaner and simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,17 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
