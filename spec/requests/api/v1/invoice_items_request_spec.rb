57df6a7282c3f7c912d1d7f5f542bda53cef7186
ClaireBvs <clairembeauvais@gmail.com>
2018-08-16 00:07:15 -0600

    add test find by param invoice item

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -31,4 +31,21 @@ describe "Invoice Items API" do
       expect(invoice_item["id"]).to eq(invoice_item_id)
     end
   end
+  context 'GET /api/v1/invoice_items/find?parameters' do
+    it "can find invoice item by id" do
+      merchant = create(:merchant)
+      customer = create(:customer)
+      item = create(:item, merchant_id: merchant.id)
+      invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      invoice_item_1 = create(:invoice_item, id: 1, item_id: item.id, invoice_id: invoice.id)
+      invoice_item_2 = create(:invoice_item, id: 2, item_id: item.id, invoice_id: invoice.id)
+
+      get "/api/v1/invoice_items/find?id=2"
+
+      invoice_item = JSON.parse(response.body)
+
+      expect(response).to be_successful
+      expect(invoice_item["id"]).to eq(invoice_item_2.id)
+    end
+  end
 end
